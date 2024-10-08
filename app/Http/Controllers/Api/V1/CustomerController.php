221036f5ffd3e84197ea982df311b9671246d4ae
khanhnq <khanhnq@runsystem.net>
2024-10-08 11:09:33 +0700

    feat: filtering more data

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -8,15 +8,24 @@ use App\Models\Customer;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\CustomerCollection;
 use App\Http\Resources\V1\CustomerResource;
+use Illuminate\Http\Request;
+use App\Services\V1\CustomerQuery;
 
 class CustomerController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        return CustomerCollection::make(Customer::paginate());
+        $filter = new CustomerQuery();
+        $queryItems =  $filter->transform($request); //['column', 'operator', 'value']
+
+        if (count($queryItems) > 0) {
+            return new CustomerCollection(Customer::where($queryItems)->paginate());
+        } else {
+            return new CustomerCollection(Customer::paginate());
+        }
     }
 
     /**
